c196ce04a6b0c29a76483f9db1c868d9c1522366
7Tech <119029097+Se1TECH@users.noreply.github.com>
2023-05-05 20:43:46 +0530

    Add files via upload

---
+++ Pro1.py
@@ -0,0 +1,8 @@
+# IF else in Python
+# Que: Check user is eligible for voting or not.
+age = int(input("Enter Your Age: "))
+if age > 18:
+    print("You are Eligible for Voting")
+else:
+    print("You are Not Eligible for Voting") 
+
