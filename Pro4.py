05f24818d570973fa1b34078f7ead3318f11ddb6
7Tech <119029097+Se1TECH@users.noreply.github.com>
2023-05-09 20:23:52 +0530

    Add files via upload

---
+++ Pro4.py
@@ -0,0 +1,10 @@
+#while Loop in Python
+# Que: Sum of natural numbers in which is enter by user
+num = int(input("Enter Number to perform sum: "))
+sum = 0
+c=0
+while c <= num:
+    sum += c
+    c+=1
+
+print("Sum of Natural Numbers: ", sum)
