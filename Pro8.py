631935dfbc5c5348eb5e918b29391377f821e517
7Tech <119029097+Se1TECH@users.noreply.github.com>
2023-05-13 14:09:11 +0530

    Add files via upload

---
+++ Pro8.py
@@ -0,0 +1,7 @@
+#print string in reverse in python
+
+str1 = "Hello How are You"
+
+print("String in reverse: ", str1[::-1])
+print("String str1[-4:-1]: ", str1[-4:-1])
+print("String str1[0:-1]: ", str1[0:-1])
