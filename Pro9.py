9e1c0b3bd291a7fcb0e7e621bf2d3961bcdbb30c
7Tech <119029097+Se1TECH@users.noreply.github.com>
2023-05-14 12:47:16 +0530

    Add files via upload

---
+++ Pro9.py
@@ -0,0 +1,15 @@
+#list in python
+
+list1 = [1,"John doe",100000]
+print("Id of Employee: ",list1[0])
+print("Name of Employee: ",list1[1])
+print("Salary of Employee: ",list1[2])
+
+print("Full List: ",list1)
+
+list1[2] = 200000
+print("full List Updated: ",list1)
+
+list2 = [10,20,30,40,50,60]
+print("list2[1:5]: ", list2[1:5])
+print("list2[-1:-5]: ", list2[-5:-2])
