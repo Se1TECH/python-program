15fd162f39af04e0528675192c5e389f8161be0c
7Tech <119029097+Se1TECH@users.noreply.github.com>
2023-05-15 20:37:38 +0530

    Add files via upload

---
+++ Pro10.py
@@ -0,0 +1,9 @@
+#tuple in python
+tuple1 = 4,5,6,7,"Hello"
+
+print("Full Tuple: " , tuple1)
+print("tuple1[2]: ", tuple1[2])
+print("tuple1[-1]: ", tuple1[-1])
+print("tuple1[-4:-1]: ", tuple1[-4:-1])
+print("tuple1[1:5]: ", tuple1[1:5])
+
