70690ac29de020a98753f1dc2cf9611548f5985f
7Tech <119029097+Se1TECH@users.noreply.github.com>
2023-05-19 11:16:02 +0530

    Add files via upload

---
+++ Pro12.py
@@ -0,0 +1,11 @@
+#function in python
+
+def square(num):
+    return num**2
+
+def cube(num):
+    return num**3
+
+num1 = int(input("Enter Number for square and cube: "))
+print("Your sqaure of number is : ", square(num1))
+print("Your Cube of number is : ", cube(num1))
