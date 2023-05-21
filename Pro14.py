9d9df9f3be86503de747130d7eb10646388cdf60
7Tech <119029097+Se1TECH@users.noreply.github.com>
2023-05-21 10:12:17 +0530

    Add files via upload

---
+++ Pro14.py
@@ -0,0 +1,6 @@
+# Addition of two numbers Entered by user in python
+
+num1 = int(input("Enter 1st Number for addition: "))
+num2 = int(input("Enter 2nd Number for addition: "))
+
+print("Addition of two number is: ", (num1+num2))
