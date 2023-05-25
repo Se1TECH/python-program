3043e66426b546569a2bbb83cb9a3306bc354ab5
7Tech <119029097+Se1TECH@users.noreply.github.com>
2023-05-25 15:33:58 +0530

    Add files via upload

---
+++ Pro18.py
@@ -0,0 +1,10 @@
+# Find area of circle,rectangle and square
+
+radius = int(input("Enter Radius of circle: "))
+rHeight = int(input("Enter Height of rectangle: ")) 
+rWidth = int(input("Enter width of rectangle: ")) 
+sWidth = int(input("Enter width of Square: ")) 
+
+print("Area of Circle is: " , 3.14*radius*radius)
+print("Area of Rectangle is: " , rHeight*rWidth)
+print("Area of Square is: " , 4*sWidth)
