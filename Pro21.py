1489a950d1f2b8032a70423a8f60e548b81749f6
7Tech <119029097+Se1TECH@users.noreply.github.com>
2023-05-28 09:34:03 +0530

    Add files via upload

---
+++ Pro21.py
@@ -0,0 +1,7 @@
+# Get OS name, platform and release information
+import platform
+import os
+
+print("Name of operating System: ", os.name)
+print("\nName of the OS System: ", platform.system())
+print("\nVersion of the operating System: ", platform.release())
