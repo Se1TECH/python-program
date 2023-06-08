ddc4c0fbd975e9be410939308a9eff64304ae4f8
7Tech <119029097+Se1TECH@users.noreply.github.com>
2023-06-08 12:44:30 +0530

    Add files via upload

---
+++ Pro30.py
@@ -0,0 +1,7 @@
+# Write a Python program to convert the bytes
+# in a given string to a list of integers in python.
+
+str1 = b'This is byte string'
+
+print("\nConvert byter strng to integer: ")
+print(list(str1))
